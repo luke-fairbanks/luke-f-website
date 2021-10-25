3d97b1e84d3f9dbcd21b784eee9f9f96872d6071
luke-fairbanks <52293458+luke-fairbanks@users.noreply.github.com>
2021-10-25 13:47:31 -0600

    loader

--- script/portfolioPage.js
+++ script/portfolioPage.js
@@ -1,5 +1,4 @@
 //handle the loading icon until page loads
-$(window).load(function() {
-    // Animate loader off screen
-    $(".se-pre-con").fadeOut("slow");;
-});
+$(window).on("load",function(){
+    $(".loading").fadeOut("slow")
+})
