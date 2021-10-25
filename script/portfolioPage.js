4c9f629316482bc1d2a4e759551c76367b4c6c75
luke-fairbanks <52293458+luke-fairbanks@users.noreply.github.com>
2021-10-25 14:07:30 -0600

    top of page

--- script/portfolioPage.js
+++ script/portfolioPage.js
@@ -1,6 +1,7 @@
+//top of page 
+window.scroll({top:0})
 //handle the loading icon until page loads
-$(window).on("load",function(){
-    window.scroll({top:0})
+$(window).on("load",function(){ 
     $(".loading-wrapper").fadeOut("slow");
     $('body, html').css('overflow','visible');
 })
