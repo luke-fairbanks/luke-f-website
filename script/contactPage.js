4689b0739e83b4c772dcb668923f4f61331bbd7c
luke-f <52293458+luke-fairbanks@users.noreply.github.com>
2021-10-26 18:19:27 -0600

    yuh

--- script/contactPage.js
+++ script/contactPage.js
@@ -1,4 +1,4 @@
 function myWebsiteRedirect(){
     document.getElementById('website-meme').innerHTML = "what did you expect lol";
-    window.location.href = "../index.html";
+    window.location.href = "../../";
 }
